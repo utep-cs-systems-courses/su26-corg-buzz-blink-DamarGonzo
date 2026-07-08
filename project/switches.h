64e80721dac6642bec9fe5840d30b09f72c2eb9e
DamarGonzo <294263315+DamarGonzo@users.noreply.github.com>
2026-07-07 19:08:31 -0600

    Add switch definitions and function declarations

--- project/switches.h
+++ project/switches.h
@@ -1,1 +1,15 @@
+#ifndef switches_included
+#define switches_included
 
+#define SW1 BIT0
+#define SW2 BIT1
+#define SW3 BIT2
+#define SW4 BIT3
+#define SWITCHES (SW1 | SW2 | SW3 | SW4)
+
+void switch_init();
+void switch_interrupt_handler();
+
+extern int switch_state_down, switch_state_changed;
+
+#endif
