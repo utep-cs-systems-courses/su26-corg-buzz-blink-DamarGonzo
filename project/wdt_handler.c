04088c89c597bfed0ebe2f3c5d86c288dd7b6a7c
DamarGonzo <294263315+DamarGonzo@users.noreply.github.com>
2026-07-07 19:13:59 -0600

    Add WDT interrupt handler for LED dimming
    Implement watchdog timer interrupt handler for LED control.

--- project/wdt_handler.c
+++ project/wdt_handler.c
@@ -1,1 +1,12 @@
+#include <msp430.h>
+#include "stateMachines.h"
+#include "led.h"
 
+void __interrupt_vec(WDT_VECTOR) WDT() {
+  static int blink_count = 0;
+  if (++blink_count >= 1) {
+    dimming_logic();
+    led_update();
+    blink_count = 0;
+  }
+}
