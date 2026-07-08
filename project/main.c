30d78e1062e9ad6196f3f728ed36251d50a9c524
DamarGonzo <294263315+DamarGonzo@users.noreply.github.com>
2026-07-07 19:04:04 -0600

    Initialize main function and peripherals

--- project/main.c
+++ project/main.c
@@ -1,1 +1,16 @@
+#include <msp430.h>
+#include "libTimer.h"
+#include "led.h"
+#include "switches.h"
+#include "buzzer.h"
+#include "stateMachines.h"
 
+int main(void) {
+  configureClocks();
+  switch_init();
+  led_init();
+  buzzer_init();
+  enableWDTInterrupts();
+
+  or_sr(0x18);  // CPU off, GIE on
+}
