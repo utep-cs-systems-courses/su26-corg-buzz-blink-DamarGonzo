424600d8acf60246bf1e23e879f1cb07396c8c83
DamarGonzo <294263315+DamarGonzo@users.noreply.github.com>
2026-07-07 19:09:16 -0600

    Implement switch initialization and interrupt handling
    Initialize switch settings and handle interrupts for button presses.

--- project/switches.c
+++ project/switches.c
@@ -1,1 +1,30 @@
+#include <msp430.h>
+#include "switches.h"
+#include "stateMachines.h"
 
+int switch_state_down, switch_state_changed;
+
+static char switch_update_interrupt_sense() {
+  char p2val = P2IN;
+  P2IES |= (p2val & SWITCHES);
+  P2IES &= (p2val | ~SWITCHES);
+  return p2val;
+}
+
+void switch_init() {
+  P2REN |= SWITCHES;
+  P2IE |= SWITCHES;
+  P2OUT |= SWITCHES;
+  P2DIR &= ~SWITCHES;
+  switch_update_interrupt_sense();
+}
+
+void switch_interrupt_handler() {
+  char p2val = switch_update_interrupt_sense();
+  
+  // Buttons are active low
+  if (!(p2val & SW1)) state_advance(1);
+  else if (!(p2val & SW2)) state_advance(2);
+  else if (!(p2val & SW3)) state_advance(3);
+  else if (!(p2val & SW4)) state_advance(4);
+}
