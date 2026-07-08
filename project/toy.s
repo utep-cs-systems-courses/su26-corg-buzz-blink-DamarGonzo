6b0503579041dbd875efcec0df4dec3e36456cba
DamarGonzo <294263315+DamarGonzo@users.noreply.github.com>
2026-07-07 19:13:00 -0600

    Add assembly function to toggle red LED state

--- project/toy.s
+++ project/toy.s
@@ -1,1 +1,11 @@
+    .arch msp430
+    .text
 
+    .global toggle_red_asm
+    .extern red_on
+    .extern led_changed
+
+toggle_red_asm:
+    xor.b #1, &red_on      ; Toggle the red_on variable
+    mov.b #1, &led_changed ; Set led_changed to true
+    ret
