982221c041cf4a4a36312c21bfeb87df4d8bf638
DamarGonzo <294263315+DamarGonzo@users.noreply.github.com>
2026-07-07 19:13:27 -0600

    Implement PORT2 interrupt handler
    Add interrupt handler for PORT2 to manage switch events.

--- project/p2_handler.c
+++ project/p2_handler.c
@@ -1,1 +1,9 @@
+#include <msp430.h>
+#include "switches.h"
 
+void __interrupt_vec(PORT2_VECTOR) Port_2() {
+  if (P2IFG & SWITCHES) {
+    P2IFG &= ~SWITCHES;
+    switch_interrupt_handler();
+  }
+}
