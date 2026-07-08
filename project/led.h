5de1a8e9a3d03c850e0b65efa56b66933e7d577a
DamarGonzo <294263315+DamarGonzo@users.noreply.github.com>
2026-07-07 19:04:38 -0600

    Add LED definitions and function declarations

--- project/led.h
+++ project/led.h
@@ -1,1 +1,15 @@
+/* led.h */
+#ifndef led_included
+#define led_included
 
+#define LED_RED BIT0               // P1.0
+#define LED_GREEN BIT6             // P1.6
+#define LEDS (BIT0 | BIT6)
+
+void led_init();
+void led_update();
+
+extern unsigned char red_on, green_on;
+extern unsigned char led_changed;
+
+#endif
